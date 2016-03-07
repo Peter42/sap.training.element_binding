24b58afd891a76f66cd283423f63b4ab11201bc8
kicker123 <chriskne@gmail.com>
2016-03-07 10:03:56 +0000

    initial commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("sap.training.controller.Main", {
+
+		onInit: function() {
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("json/data.json");
+			this.getView().setModel(oModel);
+		},
+
+		onCarrierSelectionChange: function(oEvent) {
+			var oListItem = oEvent.getParameter("listItem");
+			var sPath = oListItem.getBindingContext().getPath();
+			var oTable = this.getView().byId("idConnTable");
+			oTable.bindElement(sPath);
+		}
+
+	});
+});
